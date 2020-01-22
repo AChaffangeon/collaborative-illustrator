5b8678f70ff6c429b69c4e5fc46bb86a849dc2ec
AChaffangeon <chaffangeon.adrien@gmail.com>
2020-01-22 14:43:27 +0100

    Update Peer.ts

--- src/ts/Peer2Peer/Peer.ts
+++ src/ts/Peer2Peer/Peer.ts
@@ -25,21 +25,7 @@ const configuration = {
             credential: 'azer2711',
             username: 'adrichou42@gmail.com'
         },
-        {
-            urls: 'turn:numb.viagenie.ca',
-            credential: 'muazkh',
-            username: 'webrtc@live.com'
-        },
-        {
-            urls: 'turn:192.158.29.39:3478?transport=udp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
-        },
-        {
-            urls: 'turn:192.158.29.39:3478?transport=tcp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
-        },
+        // Below TURN servers where found on https://gist.github.com/sagivo/3a4b2f2c7ac6e1b5267c2f1f59ac6c6b
         {
             urls: 'turn:turn.bistri.com:80',
             credential: 'homeo',
